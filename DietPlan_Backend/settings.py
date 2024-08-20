cd01c453d34bffafd57591cca99a9c0377ad2665
CemHarput <cemharput@gmail.com>
2024-08-20 18:55:33 +0300

    refactor:s3 parameter

--- DietPlan_Backend/settings.py
+++ DietPlan_Backend/settings.py
@@ -140,7 +140,10 @@ else:
         'CacheControl': 'max-age=94608000',
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     }
-    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+    STATICFILES_LOCATION='static'
+    AWS_LOCATION = 'static'
+    AWS_S3_CUSTOM_DOMAIN = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     STATIC_ROOT = STATIC_URL
 
     MEDIA_LOCATION = 'media'
